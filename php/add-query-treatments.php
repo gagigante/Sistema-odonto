ec1aca34f7dd16a0db63a547fc0fc1db5e396752
Gabriel Gigante <gabriel_gigante@outlook.com>
2019-12-25 16:27:27 -0300

    Tela de adição de consultas

---
+++ php/add-query-treatments.php
@@ -0,0 +1,37 @@
+<?php	
+	require "conexao.php";
+
+    $treatmentId = $_POST['treatmentId'];
+
+    if(!empty($_POST['data'])) {
+        $treatmentsData = $_POST['data'];
+    } else {
+        $treatmentsData = array();
+    }
+    
+    $idLogin = $_SESSION['idUsuario'];    
+
+    $query = "SELECT * FROM tb03_tratamentos WHERE tb03_id = ".$treatmentId." AND tb03_idUsuario = ".$idLogin."";    
+    
+	$resultQuery = mysqli_query($conexao, $query);
+
+    if($resultQuery->num_rows>0) { 
+        
+        while ($linha = $resultQuery->fetch_assoc()){ 
+                        
+            array_push($treatmentsData, 
+                [ 
+                    'id' => $linha['tb03_id'], 
+                    'price' => $linha['tb03_preco'],
+                    'description' => $linha['tb03_descricao'],
+                    'name' => $linha['tb03_nome'],
+                    'userId' => $linha['tb03_idUsuario'],                    
+                ]                                                                
+            );                   
+        }
+    }
+    
+    mysqli_close($conexao);	
+   
+    echo json_encode($treatmentsData);
+?>
