144394a66060518f7d6ff192f1d62377f0421c4a
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-01-07 20:52:01 -0300

    Redirecionamento para https

--- php/verificaLogin.php
+++ php/verificaLogin.php
@@ -2,6 +2,13 @@
 	// if(!isset($_SESSION)){
 	// 	session_start();
 	// } 
+
+	if($_SERVER["HTTPS"] == "on")
+	{
+		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+		// exit();
+	}
+
 	if($_SESSION["logado"]==0){ 
 		header("Location: login.php");
 	}	
