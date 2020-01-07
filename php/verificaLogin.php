5736dfbd33ff0884837e42ceee4fb190279d110e
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-01-07 20:55:48 -0300

    Ajuste

--- php/verificaLogin.php
+++ php/verificaLogin.php
@@ -3,11 +3,11 @@
 	// 	session_start();
 	// } 
 
-	if($_SERVER["HTTPS"] == "on")
-	{
-		header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
-		// exit();
-	}
+	// if($_SERVER["HTTPS"] == "on")
+	// {
+	// 	header("Location: https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"]);
+	// 	// exit();
+	// }
 
 	if($_SESSION["logado"]==0){ 
 		header("Location: login.php");
