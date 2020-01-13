7dae722cbdba5bd80aa6abd36ad54a99f6364891
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-01-13 20:12:31 -0300

    Correção de bug

--- php/select-patients.php
+++ php/select-patients.php
@@ -23,17 +23,14 @@
                         <tbody>";
 
         while ($linha = $resultadoselect->fetch_assoc()){                          
-
-            if(empty($linha["tb01_imagem"])){
-                $linha["tb01_imagem"] = "patient-default-profile-image.png";
-            }
+         
             if(empty($linha["tb01_cpf"])){
                 $linha["tb01_cpf"] = "(Sem dado)";
             }
             if(empty($linha["tb01_email"])){
                 $linha["tb01_email"] = "(Sem dado)";
             }
-            echo "<tr> ";
+            echo "<tr> ";          
             echo "<td><img class='user-avatar rounded-circle mr-2' src='assets/images/patients-profile-images/".$linha["tb01_imagem"]."' alt='User Avatar' width='55px' height='55px'></td>";
             echo "<td>".$linha["tb01_nome"]. "</td>";
             echo "<td>".$linha["tb01_cpf"]. "</td>";
