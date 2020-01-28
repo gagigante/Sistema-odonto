7ca56f14584097ce83786491a250c064b01142e1
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-01-28 20:35:29 -0300

    Corrigindo erro de consulta

--- php/add-query-treatments.php
+++ php/add-query-treatments.php
@@ -11,7 +11,7 @@
     
     $idLogin = $_SESSION['idUsuario'];    
 
-    $query = "SELECT * FROM tb03_tratamentos WHERE tb03_id = ".$treatmentId." AND tb03_idUsuario = ".$idLogin."";    
+    $query = "SELECT * FROM tb03_tratamentos WHERE tb03_id = ".$treatmentId." AND tb03_id_usuario = ".$idLogin."";    
     
 	$resultQuery = mysqli_query($conexao, $query);
 
@@ -25,7 +25,7 @@
                     'price' => $linha['tb03_preco'],
                     'description' => $linha['tb03_descricao'],
                     'name' => $linha['tb03_nome'],
-                    'userId' => $linha['tb03_idUsuario'],                    
+                    'userId' => $linha['tb03_id_usuario'],                    
                 ]                                                                
             );                   
         }
--- php/autoCompleteAddTreatment.php
+++ php/autoCompleteAddTreatment.php
@@ -5,7 +5,7 @@ $idLogin = $_SESSION['idUsuario'];
 
 $nomeComplete = filter_input(INPUT_GET, 'term', FILTER_SANITIZE_STRING);
 
-$queryselect = "SELECT * FROM tb03_tratamentos WHERE tb03_nome LIKE '%".$nomeComplete."%' AND tb03_idUsuario = '$idLogin' ORDER BY tb03_nome ASC LIMIT 7";
+$queryselect = "SELECT * FROM tb03_tratamentos WHERE tb03_nome LIKE '%".$nomeComplete."%' AND tb03_id_usuario = '$idLogin' ORDER BY tb03_nome ASC LIMIT 7";
 $resultadoselect = $conexao->query($queryselect);
 
 
