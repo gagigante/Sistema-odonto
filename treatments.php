0a2c654955e9399e7b05abed72c1725fbc1bf0a1
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-01-28 21:30:55 -0300

    Atualizando ano do footer no Cat. Tratamentos

--- treatments.php
+++ treatments.php
@@ -214,7 +214,7 @@
                     </div>
                 </div>
                 <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
-                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2019
+                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2020
                         <a target="_blank" href="https://decadatech.com" rel="nofollow">Decada Technology</a>
                     </span>
                 </footer>
