33c08e6c87d4eb2ae23e7ae74aa4f252bf586395
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-01 17:21:58 -0300

    Removendo variaveis de ambiente

--- php/conexao.php
+++ php/conexao.php
@@ -2,15 +2,15 @@
     //include 'verificaLogin.php';
     session_start();
 
-    // $servidor = "localhost";
-    // $usuario = "root";
-    // $senha = "";
-    // $banco = "bd_teste";
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $banco = "bd_teste";
 
-    $servidor = "mysql669.umbler.com";
-    $usuario = "plus_odonto_adm";
-    $senha = "JrMp7_4T-MpAZ";
-    $banco = "bd_plus_odonto";
+    // $servidor = "mysql669.umbler.com";
+    // $usuario = "plus_odonto_adm";
+    // $senha = "JrMp7_4T-MpAZ";
+    // $banco = "bd_plus_odonto";
 
     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
     mysqli_set_charset($conexao,"utf8");
