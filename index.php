13af69c47b4795393b96f14a4da8079d682a83fa
Gabriel Gigante <gabriel_gigante@outlook.com>
2020-03-03 22:02:16 -0300

    Ajustando dashboard para a primeira versão

--- index.php
+++ index.php
@@ -5,24 +5,22 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <title>Dashboard</title>
-    
+
     <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
     <link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
 
     <link rel="stylesheet" id="main-stylesheet" data-version="1.1.0" href="assets/libs/shards-dashboard/css/shards-dashboards.1.1.0.min.css">
-    <!-- <link rel="stylesheet" href="styles/extras.1.1.0.min.css"> -->
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
 </head>
 
 <body class="h-100">
 
-    <?php     
-        require 'php/conexao.php';
-        require 'php/verificaLogin.php';         
+    <?php
+    require 'php/conexao.php';
+    require 'php/verificaLogin.php';
     ?>
 
     <div class="container-fluid">
@@ -98,84 +96,83 @@
                 <div class="main-navbar sticky-top bg-white">
                     <!-- Main Navbar -->
                     <nav class="navbar align-items-stretch navbar-light flex-md-nowrap p-0">
-                        <form action="#" class="main-navbar__search w-100 d-none d-md-flex d-lg-flex">
-                            <div class="input-group input-group-seamless ml-3">
-                                <div class="input-group-prepend">
-                                    <div class="input-group-text">
-                                        <i class="fas fa-search"></i>
-                                    </div>
-                                </div>
-                                <input class="navbar-search form-control" type="text" placeholder="Search for something..." aria-label="Search">
-                            </div>
-                        </form>
+                        <span class="main-navbar__search w-100 d-none d-md-flex d-lg-flex"></span>
+
                         <ul class="navbar-nav border-left flex-row ">
                             <li class="nav-item border-right dropdown notifications">
-                                <a class="nav-link nav-link-icon text-center" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <!-- <a class="nav-link nav-link-icon text-center" href="#" role="button"
+                                    id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true"
+                                    aria-expanded="false">
                                     <div class="nav-link-icon__wrapper">
                                         <i class="material-icons">&#xE7F4;</i>
                                         <span class="badge badge-pill badge-danger">2</span>
                                     </div>
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-small" aria-labelledby="dropdownMenuLink">
+                                </a> -->
+                                <!-- <div class="dropdown-menu dropdown-menu-small" aria-labelledby="dropdownMenuLink">
                                     <a class="dropdown-item" href="#">
                                         <div class="notification__icon-wrapper">
                                             <div class="notification__icon">
-                                                <i class="material-icons">&#xE6E1;</i>
+                                                <i class="material-icons">schedule</i>
                                             </div>
                                         </div>
                                         <div class="notification__content">
-                                            <span class="notification__category">Analytics</span>
-                                            <p>Your website’s active users count increased by
-                                                <span class="text-success text-semibold">28%</span> in the last week. Great job!</p>
+                                            <span class="notification__category">Agenda</span>
+                                            <p>Não se esqueça de checar a agenda, você tem
+                                                <span class="text-success text-semibold">2</span> consultas hoje!</p>
                                         </div>
                                     </a>
                                     <a class="dropdown-item" href="#">
                                         <div class="notification__icon-wrapper">
                                             <div class="notification__icon">
-                                                <i class="material-icons">&#xE8D1;</i>
+                                                <i class="material-icons">monetization_on</i>
                                             </div>
                                         </div>
                                         <div class="notification__content">
-                                            <span class="notification__category">Sales</span>
-                                            <p>Last week your store’s sales count decreased by
-                                                <span class="text-danger text-semibold">5.52%</span>. It could have been worse!</p>
+                                            <span class="notification__category">Financeiro</span>
+                                            <p>Confira o painel de finanças</p>
                                         </div>
                                     </a>
-                                    <a class="dropdown-item notification__all text-center" href="#"> View all Notifications </a>
-                                </div>
+                                    <a class="dropdown-item notification__all text-center" href="#"> Ver todas as
+                                        notificações </a>
+                                </div> -->
                             </li>
                             <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-                                    <img class="user-avatar rounded-circle mr-2" src="assets/images/avatars/0.jpg" alt="User Avatar">
-                                    <span class="d-none d-md-inline-block">Sierra Brooks</span>
+                                <a class="nav-link dropdown-toggle text-nowrap px-3" data-toggle="dropdown" href="#"
+                                    role="button" aria-haspopup="true" aria-expanded="false">
+                                    <img class="user-avatar rounded-circle mr-2" src="assets/images/user-profile-images/user-default-profile-image.png"
+                                        alt="User Avatar">
+                                    <span class="d-none d-md-inline-block">Dra. Sierra Brooks</span>
                                 </a>
                                 <div class="dropdown-menu dropdown-menu-small">
-                                    <a class="dropdown-item" href="user-profile-lite.php">
-                                        <i class="material-icons">&#xE7FD;</i> Profile</a>
-                                    <a class="dropdown-item" href="components-blog-posts.php">
-                                        <i class="material-icons">vertical_split</i> Blog Posts</a>
-                                    <a class="dropdown-item" href="add-new-post.php">
-                                        <i class="material-icons">note_add</i> Add New Post</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item text-danger" href="#">
-                                        <i class="material-icons text-danger">&#xE879;</i> Logout </a>
+                                    <!-- <a class="dropdown-item" href="#">
+                                        <i class="material-icons">&#xE7FD;</i> Perfil
+                                    </a> -->
+                                    <!-- <a class="dropdown-item" href="#">
+                                        <i class="material-icons">settings_applications</i> Configurações
+                                    </a> -->
+                                    <!-- <div class="dropdown-divider"></div> -->
+                                    <a class="dropdown-item text-danger" href="php/logout.php">
+                                        <i class="material-icons text-danger">&#xE879;</i> Sair
+                                    </a>
                                 </div>
                             </li>
                         </ul>
                         <nav class="nav">
-                            <a href="#" class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left" data-toggle="collapse" data-target=".header-navbar" aria-expanded="false" aria-controls="header-navbar">
+                            <a href="#"
+                                class="nav-link nav-link-icon toggle-sidebar d-md-inline d-lg-none text-center border-left"
+                                data-toggle="collapse" data-target=".header-navbar" aria-expanded="false"
+                                aria-controls="header-navbar">
                                 <i class="material-icons">&#xE5D2;</i>
                             </a>
                         </nav>
                     </nav>
                 </div>
                 <!-- / .main-navbar -->
-                <div class="main-content-container container-fluid px-4">
-                    <!-- Page Header -->
-                    <div class="page-header row no-gutters py-4">
+                <div class="main-content-container container-fluid px-4" style="margin-top: 30px;">
+                     <!-- Page Header -->
+                     <div class="page-header row no-gutters py-4">
                         <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
-                            <span class="text-uppercase page-subtitle">Dashboard</span>
-                            <h3 class="page-title">Blog Overview</h3>
+                            <h3 class="page-title"><i class="material-icons">dashboard</i>Dashboard</h3>
                         </div>
                     </div>
                     <!-- End Page Header -->
@@ -284,7 +281,8 @@
                                             </div>
                                         </div>
                                         <div class="col-12 col-sm-6 d-flex mb-2 mb-sm-0">
-                                            <button type="button" class="btn btn-sm btn-white ml-auto mr-auto ml-sm-auto mr-sm-0 mt-3 mt-sm-0">View Full Report &rarr;</button>
+                                            <button type="button" class="btn btn-sm btn-white ml-auto mr-auto ml-sm-auto mr-sm-0 mt-3 mt-sm-0">View
+                                                Full Report &rarr;</button>
                                         </div>
                                     </div>
                                     <canvas height="130" style="max-width: 100% !important;" class="blog-overview-users"></canvas>
@@ -293,7 +291,7 @@
                         </div>
                         <!-- End Users Stats -->
                         <!-- Users By Device Stats -->
-                        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
+                        <!-- <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                             <div class="card card-small h-100">
                                 <div class="card-header border-bottom">
                                     <h6 class="m-0">Users by device</h6>
@@ -317,11 +315,10 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <!-- End Users By Device Stats -->
                         <!-- New Draft Component -->
-                        <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
-                            <!-- Quick Post -->
+                        <!-- <div class="col-lg-4 col-md-6 col-sm-12 mb-4">
                             <div class="card card-small h-100">
                                 <div class="card-header border-bottom">
                                     <h6 class="m-0">New Draft</h6>
@@ -339,11 +336,10 @@
                                     </form>
                                 </div>
                             </div>
-                            <!-- End Quick Post -->
-                        </div>
+                        </div> -->
                         <!-- End New Draft Component -->
                         <!-- Discussions Component -->
-                        <div class="col-lg-5 col-md-12 col-sm-12 mb-4">
+                        <!-- <div class="col-lg-5 col-md-12 col-sm-12 mb-4">
                             <div class="card card-small blog-comments">
                                 <div class="card-header border-bottom">
                                     <h6 class="m-0">Discussions</h6>
@@ -358,7 +354,8 @@
                                                 <a class="text-secondary" href="#">Hello World!</a>
                                                 <span class="text-muted">– 3 days ago</span>
                                             </div>
-                                            <p class="m-0 my-1 mb-2 text-muted">Well, the way they make shows is, they make one show ...</p>
+                                            <p class="m-0 my-1 mb-2 text-muted">Well, the way they make shows is, they
+                                                make one show ...</p>
                                             <div class="blog-comments__actions">
                                                 <div class="btn-group btn-group-sm">
                                                     <button type="button" class="btn btn-white">
@@ -386,7 +383,8 @@
                                                 <a class="text-secondary" href="#">Hello World!</a>
                                                 <span class="text-muted">– 4 days ago</span>
                                             </div>
-                                            <p class="m-0 my-1 mb-2 text-muted">After the avalanche, it took us a week to climb out. Now...</p>
+                                            <p class="m-0 my-1 mb-2 text-muted">After the avalanche, it took us a week
+                                                to climb out. Now...</p>
                                             <div class="blog-comments__actions">
                                                 <div class="btn-group btn-group-sm">
                                                     <button type="button" class="btn btn-white">
@@ -414,7 +412,8 @@
                                                 <a class="text-secondary" href="#">Hello World!</a>
                                                 <span class="text-muted">– 5 days ago</span>
                                             </div>
-                                            <p class="m-0 my-1 mb-2 text-muted">My money's in that office, right? If she start giving me...</p>
+                                            <p class="m-0 my-1 mb-2 text-muted">My money's in that office, right? If she
+                                                start giving me...</p>
                                             <div class="blog-comments__actions">
                                                 <div class="btn-group btn-group-sm">
                                                     <button type="button" class="btn btn-white">
@@ -442,10 +441,10 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> -->
                         <!-- End Discussions Component -->
                         <!-- Top Referrals Component -->
-                        <div class="col-lg-3 col-md-12 col-sm-12 mb-4">
+                        <div class="col-lg-4 col-md-12 col-sm-12 mb-4">
                             <div class="card card-small">
                                 <div class="card-header border-bottom">
                                     <h6 class="m-0">Top Referrals</h6>
@@ -506,32 +505,15 @@
                         <!-- End Top Referrals Component -->
                     </div>
                 </div>
-                <footer class="main-footer d-flex p-2 px-3 bg-white border-top">
-                    <ul class="nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Services</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">About</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Products</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Blog</a>
-                        </li>
-                    </ul>
-                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2018
-                        <a href="https://designrevision.com" rel="nofollow">DesignRevision</a>
+                <footer class="main-footer d-flex p-2 px-3 bg-white border-top" style="margin-top: 30px;">
+                    <span class="copyright ml-auto my-auto mr-2">Copyright © 2020
+                        <a target="_blank" href="https://decadatech.com" rel="nofollow">Decada Technology</a>
                     </span>
                 </footer>
             </main>
         </div>
     </div>
-    
+
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
@@ -544,7 +526,7 @@
     <!-- <script src="scripts/extras.1.1.0.min.js"></script> -->
 
     <script src="assets/libs/shards-dashboard/js/shards-dashboards.1.1.0.min.js"></script>
-    
+
     <script src="assets/js/dashboardFunctions.js"></script>
 </body>
 
